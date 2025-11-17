dbc4a6028469893180feb0e70e7d798b25259f60
unknown <brsina504@gmail.com>
2025-11-17 12:07:49 +0330

    snappfood

--- src/modules/basket/basket.controller.ts
+++ src/modules/basket/basket.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Delete, Get, Post } from "@nestjs/common";
 import { ApiConsumes, ApiTags } from "@nestjs/swagger";
 import { BasketService } from "./basket.service";
-import { BasketDto } from "./dto/basket.dto";
+import { BasketDto, DiscountBasketDto } from "./dto/basket.dto";
 import { UserAuth } from "src/common/decorators/auth.decorator";
 import { FormType } from "src/common/enum/form-type.enum";
 
@@ -14,15 +14,25 @@ export class BasketController {
     @Post()
     @ApiConsumes(FormType.UrlEncoded, FormType.JSON)
     addToBasket(@Body() basketDto: BasketDto) {
-        return this.basketService.addToBasket(basketDto)
+        return this.basketService.addToBasket(basketDto);
     }
-
+    @Post("/discount")
+    @ApiConsumes(FormType.JSON, FormType.JSON)
+    addDiscountToBasket(@Body() discountDto: DiscountBasketDto) {
+        return this.basketService.addDiscount(discountDto);
+    }
     @Delete()
     @ApiConsumes(FormType.UrlEncoded, FormType.JSON)
     removeFromBasket(@Body() basketDto: BasketDto) {
-        return this.basketService.removeFromBasket(basketDto)
+        return this.basketService.removeFromBasket(basketDto);
     }
-
+    @Delete("/discount")
+    @ApiConsumes(FormType.UrlEncoded, FormType.JSON)
+    removeDiscountFromBasket(@Body() discountDto: DiscountBasketDto) {
+        return this.basketService.removeDiscount(discountDto);
+    }
     @Get()
-    getBasket() { }
+    getBasket() {
+        return this.basketService.getBasket();
+    }
 }
