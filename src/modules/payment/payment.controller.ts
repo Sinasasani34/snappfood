79f0ca103a6894cfd3a593e1b9cc2db3c55a10d0
unknown <brsina504@gmail.com>
2026-01-07 13:39:01 +0330

    snappfood

--- src/modules/payment/payment.controller.ts
+++ src/modules/payment/payment.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { PaymentService } from './payment.service';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('payment')
+@ApiTags("Payment")
 export class PaymentController {
   constructor(private readonly paymentService: PaymentService) {}
 
